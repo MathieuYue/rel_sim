1bfae997ba0b030ae7b5078eec4d06daebce6cfd
MatthewYyf <matthewyueyf@gmail.com>
2025-07-03 19:03:56 +0800

    Add simulation script to manage agent interactions and scene progression
    This commit introduces a new simulation.py file that orchestrates interactions between two RelationshipAgent instances, utilizing the SceneMaster class for scene management. The script initializes agents with specific goals, progresses through a scene, and prints narrative updates along with agent actions. Additionally, a function to display scene history and state is included, enhancing the overall simulation experience.

---
+++ simulation.py
@@ -0,0 +1,58 @@
+from scene_master.scene_master import SceneMaster
+from relationship_agent.relationship_agent import RelationshipAgent
+import uuid
+import utils.general_utils as utils
+import os
+
+agent_1_path = os.path.join("sample_agents", "ryan_reynolds", "basics.json")
+agent_2_path = os.path.join("sample_agents", "blake_lively", "basics.json")
+
+
+agent_1 = RelationshipAgent(agent_1_path)
+agent_2 = RelationshipAgent(agent_2_path)
+
+sm = SceneMaster("scene_templates/vacation", agent_1, agent_2)
+sys_action = sm.initialize()
+
+agent_1.set_goal(sys_action.character_1_goal)
+agent_2.set_goal(sys_action.character_2_goal)
+print("Scene Master:")
+print(sys_action.current_scene)
+
+
+for i in range(5):
+    print("------------------------------------------------------")
+    sys_action = sm.progress()
+    sm.append_to_history(0, sys_action.narrative)
+    print("Scene Master:")
+    print(sys_action.narrative)
+    print(utils.list_to_indexed_string(sys_action.choices))
+    print("------------------------------------------------------")
+    if sys_action.character_uuid == agent_1.agent_id:
+        curr_agent = agent_1
+    elif sys_action.character_uuid == agent_2.agent_id:
+        curr_agent = agent_2
+    agent_action = curr_agent.act(sm.scene_history, sys_action.narrative, sys_action.choices)
+    sm.append_to_history(curr_agent, sys_action.choices[agent_action.action_index])
+    print(curr_agent.name)
+    print(sys_action.choices[agent_action.action_index])
+
+
+def get_info():
+    print("-----------------------------------------------------SCENE HISTORY-----------------------------------------------------")
+    print(utils.history_to_str(sm.scene_history))
+    print("-----------------------------------------------------SCENE STATE-----------------------------------------------------")
+    print(sm.scene_state)
+    print("-----------------------------------------------------AGENT 1-----------------------------------------------------")
+    print(agent_1.description)
+    print("-----------------------------------------------------AGENT 2-----------------------------------------------------")
+    print(agent_2.description)
+
+# get_info()
+
+# while True:
+#     res = sm.progress()
+
+
+# 
+# while(True):
